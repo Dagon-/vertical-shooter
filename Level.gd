bef7881e4e2dfbd0a38aac9c79b7600d32f17878
Dagon <dagoncal@gmx.com>
2020-05-16 13:48:14 +0100

    endless scrolling and player movement

---
+++ Level.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+# How fast the level is scrolling
+var speed = 80
+var bg_lenght = 768
+
+var backgrounds = []
+
+func _ready():
+	var child_list
+
+	child_list = get_children()
+	for node in child_list:
+		if node is TileMap:
+			backgrounds.append(node)
+
+func _process(delta):
+
+	for node in backgrounds:
+		# scroll the node
+		node.position.y += speed * delta
+		# If the node is behind the player move it to the top
+		if node.position.y > bg_lenght:
+			node.position.y -= bg_lenght * 3
+			#node.rotate(3.1415926536)
+			
---
+++ Player.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta):
+	# The player's movement vector.
+	# This also resets the velocity to (0, 0)
+	#screen_size = get_viewport_rect()
+	var velocity = Vector2()  
+	# Right
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		$AnimatedSprite.play("right")
+	elif Input.is_action_just_released("ui_right"):
+		$AnimatedSprite.play("right", true) # play in reverse
+	# Left
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		$AnimatedSprite.play("left")
+	elif Input.is_action_just_released("ui_left"):
+		$AnimatedSprite.play("left", true) # play in reverse
+	# Down
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	# Up
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
