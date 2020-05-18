0543623a930734390362ed2b3116f8e7f7c87982
Dagon <dagoncal@gmx.com>
2020-05-18 22:06:01 +0100

    add mob collisions

--- Mob1.gd
+++ Mob1.gd
@@ -4,20 +4,34 @@ var max_speed = 150
 var screen_size
 var rng = RandomNumberGenerator.new()
 
-
 func _ready():
 	rng.randomize()
 	screen_size = get_viewport_rect().size
+
 	# Make sure the mobs spawn fully inside the viewport
 	var spawn_area = Vector2(40, screen_size.x - 40)
 	$AnimatedSprite.playing = true
-	select_path(spawn_area)
+	select_spawn(spawn_area)
 	
 func _process(delta):
 	position.y += max_speed * delta
 
-func select_path(spawn_area):
+func select_spawn(spawn_area):
 	position.x = rng.randi_range(spawn_area.x, spawn_area.y)
-#
+
 func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Mob1_area_entered(area):
+	# Disable collions
+	$CollisionShape2D.set_deferred("disabled", true)
+
+	# Stop processing in _process. 
+	set_process(false)
+
+	$AnimatedSprite.animation = "explode"
+	$AnimatedSprite.play()
+	$DespawnTimer.start()
+
+func _on_DespawnTimer_timeout():
 	queue_free()
--- Player.gd
+++ Player.gd
@@ -3,15 +3,18 @@ extends Area2D
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
+signal PlayerDestroyed
+
+
 func _ready():
 	screen_size = get_viewport_rect().size
-
 
 func _process(delta):
 	# The player's movement vector.
 	# This also resets the velocity to (0, 0)
 	#screen_size = get_viewport_rect()
 	var velocity = Vector2()  
+
 	# Right
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -30,11 +33,25 @@ func _process(delta):
 	# Up
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-		
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-
 
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_area_entered(area):
+	# Disable collions
+	$CollisionShape2D.set_deferred("disabled", true)
+
+	# Stop processing in _process.
+	# This is mainly to prevent keyboard input 
+	set_process(false)
+
+	$AnimatedSprite.animation = "explode"
+	$AnimatedSprite.play()
+	
+	emit_signal("PlayerDestroyed")
+	#queue_free()
