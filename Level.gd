c9af8449e63196959ffa6b0e80cc0a9073371ccb
Dagon <dagoncal@gmx.com>
2020-05-31 10:41:16 +0100

    Updates graphics for level, mob

--- Level.gd
+++ Level.gd
@@ -1,31 +1,19 @@
 extends Node2D
 
 # How fast the level is scrolling
-var speed = 80
+var speed = 2.5
 var bg_lenght = 768
 var backgrounds = []
 var Mob1
 
 func _ready():
-	var child_list
-
-	# Add tilesets to list
-	child_list = get_children()
-	for node in child_list:
-		if node is TileMap:
-			backgrounds.append(node)
 
 	# Load sauser mob
 	Mob1 = load("res://scenes/mob_saucer.tscn")
 	
 func _process(delta):
-
-	for node in backgrounds:
-		# scroll the node
-		node.position.y += speed * delta
-		# If the node is behind the player move it to the top
-		if node.position.y > bg_lenght:
-			node.position.y -= bg_lenght * 3
+	pass
+	
 
 func _on_MobTimer_timeout():
 	var saucer = Mob1.instance()
---
+++ middle_layer.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+# How fast the level is scrolling
+var speed = 4
+var bg_lenght = 768
+var backgrounds = []
+
+func _ready():
+	var child_list
+
+	# Add tilesets to list
+	child_list = get_children()
+	for node in child_list:
+		if node is Sprite:
+			backgrounds.append(node)
+
+func _process(delta):
+	
+	for node in backgrounds:
+			# scroll the node
+			node.position.y += speed * delta
+			# If the node is behind the player move it to the top
+			if node.position.y > bg_lenght:
+				node.position.y -= bg_lenght * 3
